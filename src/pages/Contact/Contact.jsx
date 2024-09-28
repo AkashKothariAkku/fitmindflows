baad43541dd7d91889e7411760634a823394d99b
lakhan0007 <lakhansinghrathore2001@gmail.com>
2024-09-28 16:26:55 +0530

    Create a contact us page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Top from "../../components/contactComponents/Top";
 
 function Contect() {
   return (
     <>
       <Header />
+        <Top/>
       <Footer />
     </>
   );
